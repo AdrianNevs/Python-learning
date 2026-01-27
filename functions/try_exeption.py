d655afe494814ea282c6ca90cfcc602cf91d1119
AdrianNeves <adrian.nevs.dias@gmail.com>
2026-01-27 05:15:59 -0300

    Create try_exeption.py

---
+++ functions/try_exeption.py
@@ -0,0 +1,73 @@
+import os
+lista = []
+
+def error_dividir_zero(x , y):
+    
+    if isinstance (x,str) or isinstance(y,str):
+        raise TypeError('str caceta nao e int') #cria proprio erro e renomeia
+    else:
+        if x  == 0 or y == 0:
+            raise ZeroDivisionError('nao se divide 0')
+        
+error_dividir_zero(10 , 0)
+
+
+
+try:
+    x = 10
+    y = 0
+    print('texto' + x)
+    division = x / y
+    #z = 12
+    #sum(x,z)
+    print('valor'[1000])
+except (ZeroDivisionError , NameError) as error:
+    print('ocorreu erro')
+    print(error.__class__.__name__) # mostrar seu repesctivo nome do erro .class.name
+except (IndexError,TypeError) as error:
+    print('erro e seu nome')
+    print(error.__class__.__name__)
+except Exception as error:
+    print('erro desconhecido')
+    print(error.__class__.__name__)
+else:
+    print('nao ocorreu erros') # quando nao haver erros
+finally: 
+    print('roda mesmo que ocorra erro') # sempre executa
+    print('sempre vai executar')
+
+
+#exercicio
+
+while True:
+    #menu
+    print('==Selecione uma opcão==')
+    Peso = str(input('[i]nserir [a]pagar [l]istar [S]air')).lower()
+    #validar opcoes
+    if 'i' == Peso:
+        lista.append(input('Valor: '))
+    elif 'a' == Peso:
+        if len(lista) == 0:
+            print('lista vazia')
+        else:
+            try:
+                Peso = int(input('escolha o indice: '))
+                del lista[Peso]
+            except IndexError:
+                print('indice invalido')
+            except ValueError:
+                print('Digite um numero inteiro')
+            except Exception:
+                print('Erro desconhecido')
+    elif 'l' == Peso:
+        os.system('cls')
+        
+        if len(lista) == 0:
+            print(f'\n==nada para listar==\n')
+        else:
+            for i in range(len(lista)):
+                print(i, lista[i])
+    elif 's' == Peso:
+        break
+    else:
+        print('Digite apenas [i,a,l,s]')

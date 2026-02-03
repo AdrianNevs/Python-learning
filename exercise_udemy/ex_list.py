76d5090facd6d1e2ca31179ebd295a6fadac43a7
AdrianNeves <adrian.nevs.dias@gmail.com>
2026-02-03 11:45:29 -0300

    Update ex_list.py

--- exercise_udemy/ex_list.py
+++ exercise_udemy/ex_list.py
@@ -9,7 +9,7 @@ estado_list = ['BA', 'SP','MG','RJ']
 # Resultado
 # [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
 
-def zipper(citys):
+def zipper(citys): #usando closure
     def unir(estado):
         new_list = []
         for i,city in enumerate(citys):
@@ -21,3 +21,25 @@ def zipper(citys):
 
 unir_city_estado = zipper(city_list)
 print(unir_city_estado(estado_list))
+
+#/////////=====================================\\\\\\\\\
+#exemplo usando decorador 
+def decorador(func):
+    def interno(*values,**kwargs):
+        print('passei aqui')
+        result =  func(*values,**kwargs)
+        return result , 'Agora consigo modificar'
+    return interno
+
+@decorador
+def zipper(cities,estado):
+    new_list = []
+    for i,city in enumerate(cities):
+        if i >= len(estado):
+            break
+        new_list.append((city,estado[i]))
+    return new_list
+
+
+unir_city_estado = zipper(city_list,estado_list)
+print(*unir_city_estado)

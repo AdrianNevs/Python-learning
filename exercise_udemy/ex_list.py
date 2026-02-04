f57f8df42b4dd3daa3bdd51da5721e7d157d2946
AdrianNeves <adrian.nevs.dias@gmail.com>
2026-02-04 01:18:24 -0300

    Atualizar o ex_list.py

--- exercise_udemy/ex_list.py
+++ exercise_udemy/ex_list.py
@@ -5,22 +5,22 @@
 # Use todos os valores da menor lista.
 # Ex.:
 city_list = ['Salvador', 'Ubatuba', 'Belo Horizonte']
-estado_list = ['BA', 'SP','MG','RJ']
+state_list = ['BA', 'SP','MG','RJ']
 # Resultado
 # [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
 
 def zipper(citys): #usando closure
-    def unir(estado):
+    def unir(state):
         new_list = []
         for i,city in enumerate(citys):
             if i >= len(estado):
                 break
-            new_list.append((city,estado[i]))
+            new_list.append((city,state[i]))
         return new_list
     return unir
 
-unir_city_estado = zipper(city_list)
-print(unir_city_estado(estado_list))
+unir_city_state = zipper(city_list)
+print(unir_city_state(state_list))
 
 #/////////=====================================\\\\\\\\\
 #exemplo usando decorador 
@@ -32,22 +32,22 @@ def decorador(func):
     return interno
 
 @decorador
-def zipper(cities,estado):
+def zipper(cities,state):
     new_list = []
     for i,city in enumerate(cities):
-        if i >= len(estado):
+        if i >= len(state):
             break
-        new_list.append((city,estado[i]))
+        new_list.append((city,state[i]))
     return new_list
 
 
-unir_city_estado = zipper(city_list,estado_list)
-print(*unir_city_estado)
+unir_city_state = zipper(city_list,state_list)
+print(*unir_city_state)
 
 #/////////=====================================\\\\\\\\\
 #maneira mais simples
 #uso de if ternario e list comprehension
-def zipper(city_l1, estado_l2):
-    size_list = len(city_l1) if len(city_l1) < len(estado_l2) else len(estado_l2) # ou size_list = min(len(city_l1),len(estado_l2))
-    return  [(city_l1[i],estado_l2[i]) for i in range(size_list)]
-print(zipper(city_list,estado_list))
+def zipper(city_l1, state_l2):
+    size_list = len(city_l1) if len(city_l1) < len(state_l2) else len(state_l2) # ou size_list = min(len(city_l1),len(estado_l2))
+    return  [(city_l1[i],state_l2[i]) for i in range(size_list)]
+print(zipper(city_list,state_list))

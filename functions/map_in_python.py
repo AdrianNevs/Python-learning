8ca60df77b9300bd9da9f4c453f5638bae73a94d
AdrianNeves <adrian.nevs.dias@gmail.com>
2026-02-08 22:14:02 -0300

    Criar map_in_python.py

---
+++ functions/map_in_python.py
@@ -0,0 +1,33 @@
+# aprendendo utilizacao do map
+# calcular taxas da lista
+list_price = [100,200,300,400]
+def add_taxes(value):
+    if value >= 200:
+        new_price = value * 1.1
+    else:
+        new_price = value * 2.2
+    return new_price
+
+print(list(map(add_taxes,list_price)))
+
+#def que adiciona * a cada frase
+#utilizando map para passar em cada item da lista
+list_phrase = ['adicionando','varios','pontinhos']
+def add_msg(msg):
+    new_msg = f'{msg}*'
+    return new_msg
+
+new_phrase = list(map(add_msg,list_phrase))
+print(*new_phrase)
+
+# def saudar utiliza uma lista de nomes
+# retorna uma lista com todos nomes com as saudações
+# usando sep='\n' para separar cada saudação
+list_name = ['adrian','moises','josé']
+def greeting(msg):
+    new_msg = f'{msg} Good morning'
+    return new_msg
+
+greeting = list(map(greeting,list_name))
+print(*greeting,sep='\n')
+

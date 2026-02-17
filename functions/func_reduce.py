541468238f0c565bd2dee3a3c802ffc677bc8781
AdrianNeves <adrian.nevs.dias@gmail.com>
2026-02-16 22:15:02 -0300

    Implement matrix zero and one counting with reduce
    Added exercises to count zeros and ones in a matrix using reduce.

--- functions/func_reduce.py
+++ functions/func_reduce.py
@@ -1,4 +1,8 @@
 from functools import reduce
+# ======================================
+# INTRODUÇÃO AO REDUCE
+# ====================================
+#primeiro passos
 products = [
     {'nome': 'Produto 5', 'preco': 10.00},
     {'nome': 'Produto 1', 'preco': 22.32},
@@ -32,9 +36,54 @@ total = reduce(
 )
 print(total)
 
+# ======================================
+# Exercícios aprendendo a usar o reduce
+# ======================================
+#Faça um programa que leia números inteiros m e n e os elementos de uma matriz A de números 
+#inteiros de dimensão m x n e conte o número de elementos que são iguais a zero utilizando reduce
+
+m = int(input('Informe a quantidade de linhas (m): '))
+n = int(input('Informe a quantidade de colunas (n): '))
+matriz_numbers = [[int(input(f'Digite o elemento [{i}][{j}]: ')) for j in range(n)] for i in range(m)] # Criação da matriz List Comprehension
+
+def count_zero(accumulate,matriz):
+    zero = matriz.count(0)
+    return accumulate + zero
+    
+reduce_zero = reduce(count_zero,matriz_numbers,0)
+print('Quantidade de zeros',reduce_zero)
+
+#=====================================
+#forma mais profissional e facil de dbugar:
+def count_zero(accumulate,matriz):
+    zero = reduce(lambda acc,value: acc + 1 if value == 0 else acc,matriz,0)
+    return accumulate + zero
+    
+reduce_zero = reduce(count_zero,matriz_numbers,0)
+print('Quantidade de zeros',reduce_zero)
+
+
+
 #/////////////=====================================\\\\\\\\\\\\\\\\
 #/////////////=====================================\\\\\\\\\\\\\\\\
-#exercicios aprededo a usar o reduce 
+# line_matriz pega cada linha da matriz.
+# acc é o acumulador geral (contador total principal), iniciado em 0.
+# Para cada linha, o reduce interno conta quantos valores iguais a 1 existem.
+# value_line representa cada elemento dentro da linha atual.
+# zero_count é o acumulador da linha, responsável por somar os valores encontrados.
+# O operador ternário adiciona 1 ao contador quando o valor é 1,
+# caso contrário adiciona 0, mantendo o valor atual.
+# O resultado do reduce interno (quantidade de 1 na linha)
+# é somado ao acc no reduce externo.
+# No final, definimos a matriz que será percorrida e o valor inicial do acumulador.
+# imagine como se primeiro reduce fosse um for i ,
+# eo segundo fosse reduce fosse o for j 
+count_one = reduce(lambda acc,line_matriz:acc + reduce(lambda zero_count,value_line: zero_count + (1 if value_line == 1 else 0),line_matriz,0),matriz_numbers,0) 
+print('quantidade de 1 = ',count_one)
+
+
+#/////////////=====================================\\\\\\\\\\\\\\\\
+#/////////////=====================================\\\\\\\\\\\\\\\\
 #Exercício 1
 #Crie uma função com reduce que receba uma lista de números
 #e retorne a soma apenas dos números pares.
@@ -95,35 +144,3 @@ def larger_value(accumulate,dic_products):
 
 larger = reduce(larger_value,products)
 print(larger)
-
-
-#Faça um programa que leia números inteiros m e n e os elementos de uma matriz A de números 
-#inteiros de dimensão m x n e conte o número de elementos que são iguais a zero utilizando reduce
-
-m = int(input('Informe a quantidade de linhas (m): '))
-n = int(input('Informe a quantidade de colunas (n): '))
-matriz_numbers = [[int(input(f'Digite o elemento [{i}][{j}]: ')) for j in range(n)] for i in range(m)] # Criação da matriz List Comprehension
-
-def count_zero(accumulate,matriz):
-    zero = matriz.count(0)
-    return accumulate + zero
-    
-reduce_zero = reduce(count_zero,matriz_numbers,0)
-print('Quantidade de zeros',reduce_zero)
-
-#/////////////=====================================\\\\\\\\\\\\\\\\
-#/////////////=====================================\\\\\\\\\\\\\\\\
-# line_matriz pega cada linha da matriz.
-# acc é o acumulador geral (contador total principal), iniciado em 0.
-# Para cada linha, o reduce interno conta quantos valores iguais a 1 existem.
-# value_line representa cada elemento dentro da linha atual.
-# zero_count é o acumulador da linha, responsável por somar os valores encontrados.
-# O operador ternário adiciona 1 ao contador quando o valor é 1,
-# caso contrário adiciona 0, mantendo o valor atual.
-# O resultado do reduce interno (quantidade de 1 na linha)
-# é somado ao acc no reduce externo.
-# No final, definimos a matriz que será percorrida e o valor inicial do acumulador.
-# imagine como se primeiro reduce fosse um for i ,
-# eo segundo fosse reduce fosse o for j 
-count_one = reduce(lambda acc,line_matriz:acc + reduce(lambda zero_count,value_line: zero_count + (1 if value_line == 1 else 0),line_matriz,0),matriz_numbers,0) 
-print('quatidade de 1 = ',count_one)
